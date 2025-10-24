78096564669fe2f7ef7b01eb2953f32c5890403b
usuryadevarasjoq <UdayaBhaskar.Suryadevara@cms.hhs.gov>
2025-10-24 14:50:35 -0400

    Create BeneInformation.java

---
+++ src/main/java/gov/cms/esmd/bean/response/BeneInformation.java
@@ -0,0 +1,69 @@
+package gov.cms.esmd.bean.response;
+
+public class BeneInformation
+{
+    public String mbi;
+    public String address;
+
+    public String getMbi() {
+        return mbi;
+    }
+
+    public void setMbi(String mbi) {
+        this.mbi = mbi;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getChannelOfReceivedByMAC() {
+        return channelOfReceivedByMAC;
+    }
+
+    public void setChannelOfReceivedByMAC(String channelOfReceivedByMAC) {
+        this.channelOfReceivedByMAC = channelOfReceivedByMAC;
+    }
+
+    public String address2;
+    public String city;
+    public String state;
+    public String zipCode;
+    public String channelOfReceivedByMAC;
+}
